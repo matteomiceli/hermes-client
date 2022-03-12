272e93fd16eb7ff701f012b2123800f0f0ed3d77
Ryan Collicutt <ryancollicutt22@gmail.com>
2022-03-11 23:52:38 -0800

    added quote button

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,6 +1,7 @@
 import Box from "../components/Box";
 import BoxStrike from "../components/BoxStrike";
 import Button from "../components/Button.js";
+import QuoteButton from "../components/QuoteButton";
 
 const GamePage = () => {
   return (
@@ -8,7 +9,8 @@ const GamePage = () => {
           <h1>Game</h1>
           <Box />
           <BoxStrike />
-         <Button />
+          <Button />
+          <QuoteButton />
     </>
   );
 };
