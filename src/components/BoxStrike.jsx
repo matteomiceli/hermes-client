831aa677bfd7a9dc1b6d44ff061225babe985305
Sara Wasim <sarawasim@hotmail.com>
2022-03-11 21:57:36 -0800

    styled strike box components

--- src/components/BoxStrike.jsx
+++ src/components/BoxStrike.jsx
@@ -1,7 +1,30 @@
 const BoxStrike = () => {
-    return <>
-        <div className="box-strike"></div>
-    </>;
+  const boxStyle = {
+    height: "60px",
+    width: "60px",
+    backgroundColor: "black",
+    borderRadius: "8px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "3px solid white",
+  };
+
+  const xStyle = {
+    color: "white",
+    fontSize: "80px",
+    margin: "0",
+    lineHeight: "20px",
+    height: "40px",
+  };
+
+  return (
+    <>
+      <div style={boxStyle}>
+        <p style={xStyle}>x</p>
+      </div>
+    </>
+  );
 };
 
 export default BoxStrike;
