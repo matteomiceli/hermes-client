96279ccd69533e56ab990a743cec953c28875479
Sara Wasim <sarawasim@hotmail.com>
2022-03-11 23:14:15 -0800

    styled button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const buttonStyle = {
+  background: "linear-gradient(180deg, #8241F9 0%, #4E277B 100%)",
   position: "absolute",
-  top: "50px",
-  right: "60px",
-  margin: "100px ",
-  height: "60px",
-  width: "140px",
+  height: "50px",
+  width: "220px",
+  fontSize: "18px",
+  borderRadius: "8px",
+  padding: "5px 10px",
+  border: "1px solid #150A27"
 };
 
 const Button = ({ handleClick }) => (
   <button className="btn btn-default" style={buttonStyle} onClick={handleClick}>
-    Generate new phrase
+    Generate New Quote
   </button>
 );
 
