a79b2ca355338b598f1502b248c3b5948dceae23
Sara Wasim <sarawasim@hotmail.com>
2022-03-11 21:24:01 -0800

    created components

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,7 +1,12 @@
+import Box from "../components/Box";
+import BoxStrike from "../components/BoxStrike";
+
 const GamePage = () => {
   return (
     <>
-      <h1>Game</h1>
+          <h1>Game</h1>
+          <Box />
+          <BoxStrike />
 
     </>
   );
---
+++ src/components/BoxStrike.jsx
@@ -0,0 +1,7 @@
+const BoxStrike = () => {
+    return <>
+        <div className="box-strike"></div>
+    </>;
+};
+
+export default BoxStrike;
